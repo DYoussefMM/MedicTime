6deb6921d3c1a3eabbfe38bd14adc99a75a2df87
hpisaac <herreroisaac13@gmail.com>
2024-06-12 21:17:00 -0600

    Calendario funcional hasta autentificar (12/06)

--- app/src/main/java/com/example/medictime/MainActivity.kt
+++ app/src/main/java/com/example/medictime/MainActivity.kt
@@ -4,6 +4,7 @@ package com.example.medictime
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,7 +14,9 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         val prefs = getSharedPreferences("auth_prefs", MODE_PRIVATE)
-        if (!prefs.getBoolean("is_authenticated", false)) {
+        val isAuthenticated = prefs.getBoolean("is_authenticated", false)
+
+        if (!isAuthenticated) {
             // El usuario no está autenticado, llevarlo a la AuthActivity
             onStop()
             val intent = Intent(this, AuthActivity::class.java)
@@ -24,10 +27,17 @@ class MainActivity : AppCompatActivity() {
         val btnCalendarios: Button = findViewById(R.id.btnCalendarios)
         val btnPerfil: Button = findViewById(R.id.btnPerfil)
 
+        btnCalendarios.isEnabled = isAuthenticated
+
         btnCalendarios.setOnClickListener {
-            val intent = Intent(this, CalendariosActivity::class.java)
-            startActivity(intent)
+            if (isAuthenticated) {
+                val intent = Intent(this, CalendariosActivity::class.java)
+                startActivity(intent)
+            } else {
+                Toast.makeText(this, "Debes iniciar sesion para acceder a los calendarios", Toast.LENGTH_SHORT).show()
+            }
         }
+
 
         btnPerfil.setOnClickListener {
             val intent = Intent(this, PerfilActivity::class.java)
