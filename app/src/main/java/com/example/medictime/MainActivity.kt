6c4c16695abc39cb5af67819f04f16d5c5b0e2a8
hpisaac <herreroisaac13@gmail.com>
2024-06-13 17:20:29 -0600

    Calendario funcional hasta autentificar (12/06)

--- app/src/main/java/com/example/medictime/MainActivity.kt
+++ app/src/main/java/com/example/medictime/MainActivity.kt
@@ -13,28 +13,31 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        val btnCalendarios: Button = findViewById(R.id.btnCalendarios)
+        val btnPerfil: Button = findViewById(R.id.btnPerfil)
+
         val prefs = getSharedPreferences("auth_prefs", MODE_PRIVATE)
         val isAuthenticated = prefs.getBoolean("is_authenticated", false)
 
         if (!isAuthenticated) {
-            // El usuario no está autenticado, llevarlo a la AuthActivity
-            onStop()
+            // Deshabilitar el botón btnCalendarios ya que el usuario no está autenticado
+            btnCalendarios.isEnabled = false
+            // Redirigir a AuthActivity para autenticación
             val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
             finish()
-        }
+        } else {
+            // Habilitar el botón btnCalendarios si el usuario está autenticado
+            btnCalendarios.isEnabled = true
 
-        val btnCalendarios: Button = findViewById(R.id.btnCalendarios)
-        val btnPerfil: Button = findViewById(R.id.btnPerfil)
-
-        btnCalendarios.isEnabled = isAuthenticated
-
-        btnCalendarios.setOnClickListener {
-            if (isAuthenticated) {
+            btnCalendarios.setOnClickListener {
                 val intent = Intent(this, CalendariosActivity::class.java)
                 startActivity(intent)
-            } else {
-                Toast.makeText(this, "Debes iniciar sesion para acceder a los calendarios", Toast.LENGTH_SHORT).show()
+            }
+
+            btnPerfil.setOnClickListener {
+                val intent = Intent(this, PerfilActivity::class.java)
+                startActivity(intent)
             }
         }
 
