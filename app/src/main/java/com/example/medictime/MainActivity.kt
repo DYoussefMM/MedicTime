b01ff4edf3755c39c44909d95e211603fb7159ea
hpisaac <herreroisaac13@gmail.com>
2024-06-13 22:26:22 -0600

    Esta mierda no sirvió

--- app/src/main/java/com/example/medictime/MainActivity.kt
+++ app/src/main/java/com/example/medictime/MainActivity.kt
@@ -1,10 +1,8 @@
 package com.example.medictime
 
-// MainActivity.kt
 import android.content.Intent
 import android.os.Bundle
 import android.widget.Button
-import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 
 class MainActivity : AppCompatActivity() {
@@ -13,34 +11,22 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btnCalendarios: Button = findViewById(R.id.btnCalendarios)
-        val btnPerfil: Button = findViewById(R.id.btnPerfil)
-
         val prefs = getSharedPreferences("auth_prefs", MODE_PRIVATE)
-        val isAuthenticated = prefs.getBoolean("is_authenticated", false)
-
-        if (!isAuthenticated) {
-            // Deshabilitar el botón btnCalendarios ya que el usuario no está autenticado
-            btnCalendarios.isEnabled = false
-            // Redirigir a AuthActivity para autenticación
+        if (!prefs.getBoolean("is_authenticated", false)) {
+            // El usuario no está autenticado, llevarlo a la AuthActivity
+            onStop()
             val intent = Intent(this, AuthActivity::class.java)
             startActivity(intent)
             finish()
-        } else {
-            // Habilitar el botón btnCalendarios si el usuario está autenticado
-            btnCalendarios.isEnabled = true
-
-            btnCalendarios.setOnClickListener {
-                val intent = Intent(this, CalendariosActivity::class.java)
-                startActivity(intent)
-            }
-
-            btnPerfil.setOnClickListener {
-                val intent = Intent(this, PerfilActivity::class.java)
-                startActivity(intent)
-            }
         }
 
+        val btnCalendarios: Button = findViewById(R.id.btnCalendarios)
+        val btnPerfil: Button = findViewById(R.id.btnPerfil)
+
+        btnCalendarios.setOnClickListener {
+            val intent = Intent(this, CalendariosActivity::class.java)
+            startActivity(intent)
+        }
 
         btnPerfil.setOnClickListener {
             val intent = Intent(this, PerfilActivity::class.java)
